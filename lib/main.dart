b5be3532aff1d50c8f128bae908b9822e760f06e
Nijamudeen Mohamed Sulaiman <nijamudeensulaiman@syncfusion.com>
2020-03-18 12:02:14 +0530

    sample added

---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:syncfusion_localizations/syncfusion_localizations.dart';
+
+void main() => runApp(LocalizationSupport());
+
+class LocalizationSupport extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+          SfGlobalLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en'),
+        const Locale('zh'),
+        const Locale('he'),
+        const Locale('ru'),
+        const Locale('fr', 'BE'),
+        const Locale('fr', 'CA'),
+        const Locale('ja'),
+        const Locale('de'),
+        const Locale('hi'),
+        const Locale('ar'),
+      ],
+      locale: const Locale('ar'),
+      debugShowCheckedModeBanner: false,
+      home: CustomStringLocale(),
+    );
+  }
+}
+
+class CustomStringLocale extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => ScheduleExample();
+}
+
+class ScheduleExample extends State<CustomStringLocale> {
+  CalendarView _calendarView;
+
+  @override
+  void initState() {
+    _calendarView = CalendarView.month;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return (Scaffold(
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: SfCalendar(
+              view: _calendarView,
+              monthViewSettings: MonthViewSettings(showAgenda: true),
+              dataSource: _getCalendarDataSource(),
+            ),
+          ),
+        ],
+      ),
+    ));
+  }
+}
+
+_AppointmentDataSource _getCalendarDataSource() {
+  List<Appointment> appointments = <Appointment>[];
+  appointments.add(Appointment(
+    startTime: DateTime.now(),
+    endTime: DateTime.now().add(Duration(minutes: 10)),
+    subject: 'Meeting',
+    color: Colors.blue,
+    startTimeZone: '',
+    endTimeZone: '',
+  ));
+
+  return _AppointmentDataSource(appointments);
+}
+
+class _AppointmentDataSource extends CalendarDataSource {
+  _AppointmentDataSource(List<Appointment> source) {
+    appointments = source;
+  }
+}
