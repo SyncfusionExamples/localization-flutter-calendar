e04214b22350c728dde665786a515290c671c427
Muniappan <muniappan.subramanian@syncfusiopn.com>
2022-12-20 17:21:02 +0530

    sample updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 import 'package:syncfusion_localizations/syncfusion_localizations.dart';
 
-void main() => runApp(LocalizationSupport());
+void main() => runApp(const LocalizationSupport());
 
 class LocalizationSupport extends StatelessWidget {
+  const LocalizationSupport({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       localizationsDelegates: [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         SfGlobalLocalizations.delegate,
       ],
       supportedLocales: [
-        const Locale('en'),
-        const Locale('zh'),
-        const Locale('he'),
-        const Locale('ru'),
-        const Locale('fr', 'BE'),
-        const Locale('fr', 'CA'),
-        const Locale('ja'),
-        const Locale('de'),
-        const Locale('hi'),
-        const Locale('ar'),
+        Locale('en'),
+        Locale('zh'),
+        Locale('he'),
+        Locale('ru'),
+        Locale('fr', 'BE'),
+        Locale('fr', 'CA'),
+        Locale('ja'),
+        Locale('de'),
+        Locale('hi'),
+        Locale('ar'),
       ],
-      locale: const Locale('zh'),
+      locale: Locale('zh'),
       debugShowCheckedModeBanner: false,
       home: CustomStringLocale(),
     );
@@ -36,6 +36,8 @@ class LocalizationSupport extends StatelessWidget {
 }
 
 class CustomStringLocale extends StatefulWidget {
+  const CustomStringLocale({super.key});
+
   @override
   State<StatefulWidget> createState() => ScheduleExample();
 }
@@ -48,7 +50,7 @@ class ScheduleExample extends State<CustomStringLocale> {
       body: SafeArea(
         child: SfCalendar(
           view: CalendarView.month,
-          monthViewSettings: MonthViewSettings(showAgenda: true),
+          monthViewSettings: const MonthViewSettings(showAgenda: true),
           dataSource: _getCalendarDataSource(),
         ),
       ),
@@ -60,7 +62,7 @@ _AppointmentDataSource _getCalendarDataSource() {
   List<Appointment> appointments = <Appointment>[];
   appointments.add(Appointment(
     startTime: DateTime.now(),
-    endTime: DateTime.now().add(Duration(minutes: 10)),
+    endTime: DateTime.now().add(const Duration(minutes: 10)),
     subject: '会议',
     color: Colors.blue,
     startTimeZone: '',
